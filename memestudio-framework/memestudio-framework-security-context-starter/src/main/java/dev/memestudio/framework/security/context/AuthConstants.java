057979e9790f5aa25e1e3bad24c9dc72b3f02e49
meme <meme@memestudio.dev>
2020-08-10 17:39:48 +0800

    登陆验证等

--- memestudio-framework/memestudio-framework-security-context-starter/src/main/java/dev/memestudio/framework/security/context/AuthConstants.java
+++ memestudio-framework/memestudio-framework-security-context-starter/src/main/java/dev/memestudio/framework/security/context/AuthConstants.java
@@ -8,9 +8,15 @@ public class AuthConstants {
 
     public static final String AUTH_USER_HEADER = "x-memestudio-framework-auth-user";
 
-    public static final String AUTH_USER_PERMISSIONS = "memestudio_framework_auth_user_permission";
-    
     public static final String TOKEN_HEADER = "x-token";
+
+    public static final String ACCOUNT_HEADER = "x-account";
+
+    public static final String PASSWORD_HEADER = "x-password";
+
+    public static final String SCOPE_HEADER = "x-scope";
+
+    public static final String AUTH_USER_PERMISSIONS = "memestudio_framework_auth_user_permission";
 
     public static final String AUTH_TOKEN_STORE_TOKEN_KEY = "auth_token_store_token";
 

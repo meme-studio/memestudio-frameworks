17246a79533c11f33f091651aa0d3c3bf213d15c
meme <meme@memestudio.dev>
2020-08-25 14:12:46 +0800

    修复

--- memestudio-framework/memestudio-framework-security-starter/src/main/java/dev/memestudio/framework/security/FrameworkSecurityAutoConfiguration.java
+++ memestudio-framework/memestudio-framework-security-starter/src/main/java/dev/memestudio/framework/security/FrameworkSecurityAutoConfiguration.java
@@ -5,12 +5,10 @@ import dev.memestudio.framework.security.context.AccessTypeHeaderMapper;
 import dev.memestudio.framework.security.context.PermissionProvider;
 import dev.memestudio.framework.security.context.ResourceAccessProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,43 +18,18 @@ import java.util.List;
 @RequiredArgsConstructor
 @ConditionalOnWebApplication
 @Configuration
-public class FrameworkSecurityAutoConfiguration implements WebMvcConfigurer {
-
-    private final ObjectMapper objectMapper;
-
-    @Autowired
-    private AuthUserInterceptor authUserInterceptor;
-
-    @Autowired
-    private PermissionInterceptor permissionInterceptor;
+public class FrameworkSecurityAutoConfiguration {
 
     @Bean
     public PermissionInterceptor permissionInterceptor() {
         return new PermissionInterceptor();
     }
 
-    @Lazy
     @Bean
     public AuthUserInterceptor authUserInterceptor(PermissionProvider permissionProvider,
                                                    ResourceAccessProvider resourceAccessProvider,
                                                    AccessTypeHeaderMapper accessTypeHeaderMapper) {
         return new AuthUserInterceptor(permissionProvider, resourceAccessProvider, accessTypeHeaderMapper);
-    }
-
-    @Bean
-    public AuthUserIdMethodArgumentResolver authenticationUserIdMethodArgumentResolver() {
-        return new AuthUserIdMethodArgumentResolver(objectMapper);
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(authenticationUserIdMethodArgumentResolver());
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authUserInterceptor).addPathPatterns("/**");
-        registry.addInterceptor(permissionInterceptor).addPathPatterns("/**");
     }
 
     @ConditionalOnMissingBean
@@ -75,6 +48,34 @@ public class FrameworkSecurityAutoConfiguration implements WebMvcConfigurer {
     @Bean
     public ResourceAccessProvider resourceAccessProvider() {
         return __ -> null;
+    }
+
+    @RequiredArgsConstructor
+    @Configuration
+    static class SecurityWebMvcConfigurer implements WebMvcConfigurer {
+
+        private final AuthUserInterceptor authUserInterceptor;
+
+        private final PermissionInterceptor permissionInterceptor;
+
+        private final ObjectMapper objectMapper;
+
+        @Override
+        public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+            argumentResolvers.add(authenticationUserIdMethodArgumentResolver());
+        }
+
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(authUserInterceptor).addPathPatterns("/**");
+            registry.addInterceptor(permissionInterceptor).addPathPatterns("/**");
+        }
+
+        @Bean
+        public AuthUserIdMethodArgumentResolver authenticationUserIdMethodArgumentResolver() {
+            return new AuthUserIdMethodArgumentResolver(objectMapper);
+        }
+
     }
 
 }

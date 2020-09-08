7225c56b85bdb131ba1405a7a3577f4c97877547
meme <meme@memestudio.dev>
2020-09-08 19:42:59 +0800

    优化登陆鉴权

--- memestudio-framework/memestudio-framework-security-context/src/main/java/dev/memestudio/framework/security/context/AuthConstants.java
+++ memestudio-framework/memestudio-framework-security-context/src/main/java/dev/memestudio/framework/security/context/AuthConstants.java
@@ -1,9 +1,12 @@
 package dev.memestudio.framework.security.context;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * @author meme
  * @since 2019-03-19 15:16
  */
+@UtilityClass
 public class AuthConstants {
 
     public static final String AUTH_USER_HEADER = "x-memestudio-framework-auth-user";
